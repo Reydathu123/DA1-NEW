a789f3efb72a2c0b31073feb90b0e24bccb2ace3
unknown <3.10a10.anhnguyenngocviet@gmail.com>
2026-07-18 17:45:00 +0700

    Cap nhat Brand entity

--- src/main/java/com/example/da1nhom9jav102/entity/Brand.java
+++ src/main/java/com/example/da1nhom9jav102/entity/Brand.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Brand {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
 
     @Column(name = "name", length = 250, nullable = false)
     private String name;
